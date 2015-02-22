175e7c1a2b99ca0f4c055c58b81de4475692b78a
Lieke22 <Lieke22@gmail.com>
2015-02-22 20:10:39 +0100

    javascript turotial

---
+++ myscript.js
@@ -0,0 +1,9 @@
+// Do a couple of console.logs 
+console.log("Hello from myscript.js");
+console.log("Hello again");
+
+var name = prompt("What is your name?");
+alert("Hello " + name);
+
+name = "Nick";
+console.log("The user's name is " + name);
